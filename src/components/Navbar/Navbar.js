c1165f59fe6cf07cffb87687c3085e515938a04f
cooljasonmelton <cooljasonmelton>
2020-07-23 22:11:00 -0500

    did investors nav with rotating arrow

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 // images
 import enovaLogo from '../../images/enova-logo.svg'
+import downArrow from '../../images/down-arrow.svg'
 
 const Navbar = () => {
+  const [message, setMessage] = useState(false)
+
 
 
   return (
+    
     <>
-    <div className="Navbar">
-      <img className="enova-logo" src={enovaLogo} />
-      <div className="nav-options">
-        <div> Investor Relations </div>
-        <div className="nav-company"> Company </div>
-        <div> Brands </div>
-        <div> Culture </div>
-        <div> Careers </div>
-      </div>      
-      <div className="nav-message">
-        For Display Only
+      <div className="Navbar">
+        <img className="enova-logo" src={enovaLogo} />
+        <div className="nav-options">
+          <div className="nav-investor">
+            <div> Investor Relations </div>
+              <img className="down-arrow" src={downArrow} />
+          </div>
+          <div className="nav-company"> Company </div>
+          <div> Brands </div>
+          <div> Culture </div>
+          <div> Careers </div>
+        </div>
+
+        <div className="nav-message">
+          For Display Only
+        </div>
       </div>
-    </div>
 
     </>
   );
