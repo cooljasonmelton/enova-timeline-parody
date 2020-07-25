a0abd8d155d55054b0cb3ae26eeb852316e9c4d6
cooljasonmelton <cooljasonmelton>
2020-07-25 02:14:12 -0500

    fixed z-index issue with comps so line is clickable

--- src/components/Timeline/Line/Years.js
+++ src/components/Timeline/Line/Years.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 //styling
 import './Line.css';
 
 const Years = () => {
+
+  const [active, setActive] = useState('2020')
+
+  const activeYear = year => {
+    if (year === active) return 'active'
+    else return ""
+  }
+
+
+
+
   return (
     <div className="Years">
-
       <div className="year-list">
-        <div className="year">3089</div>
-        <div className="year active">3089</div>
-        <div className="year">3089</div>
-        <div className="year">3089</div>
-        <div className="year">3089</div>
-        <div className="year">3089</div>
+        <div className={`year ${activeYear('2020')}`}
+          onClick={e => setActive(e.target.innerText)}>
+          2020
+        </div>
+        <div className={`year ${activeYear('2019')}`}
+          onClick={e => setActive(e.target.innerText)}>
+          2019
+        </div>
+        <div className="year">2019</div>
+        <div className="year">2018</div>
+        <div className="year">2017</div>
+        <div className="year">2016</div>
+        <div className="year">2015</div>
       </div>
-
     </div>
   );
 }
