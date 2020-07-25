776d2da3650c069b53dc52ddc2a8768b71121b1c
cooljasonmelton <cooljasonmelton>
2020-07-25 03:44:55 -0500

    update Card classname based on year being future or past

--- src/components/Timeline/Card/CardContainer.js
+++ src/components/Timeline/Card/CardContainer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 //styling
 import '../Timeline.css';
@@ -9,11 +9,30 @@ import CardObject from './CardObject'
 //components
 
 const CardContainer = props => {
+  const { year } = props
+
+  const prevYear = useRef(props.year);
+
+  const updatePrevYear = () => {
+    if (prevYear === props.year) return
+    prevYear.current = props.year
+  }
+
+  const animateRender = () => {
+    let numPrevYear = parseInt(prevYear.current)
+    let numYear = parseInt(year)
+    if (numPrevYear == numYear) return
+    let tense = ""
+    if (numPrevYear >= numYear) tense = "past"
+    if (numPrevYear <= numYear) tense = "future"
+    updatePrevYear()
+    return tense
+  }
 
   return (
-    <div className="CardContainer">
-      <div className="Card">
-      <p>{CardObject[props.year]}</p>
+    <div className="CardContainer" >
+      <div className={`Card ${animateRender()}`}>
+        <p>{CardObject[props.year]}</p> 
       </div>
     </div>
   );
