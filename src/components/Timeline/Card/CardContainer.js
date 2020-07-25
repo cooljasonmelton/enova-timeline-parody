7ab4cd6af53ae57d8e25b8df6e9855b99779141b
cooljasonmelton <cooljasonmelton>
2020-07-25 04:11:15 -0500

    cleaned up card code

--- src/components/Timeline/Card/CardContainer.js
+++ src/components/Timeline/Card/CardContainer.js
@@ -11,16 +11,8 @@ import CardObject from './CardObject'
 const CardContainer = props => {
   const { year } = props
 
-  // const prevYear = useRef(year);
-
   const [tense, setTense] = useState('present')
   const [sYear, setSYear] = useState(year)
-
-
-  // const updatePrevYear = () => {
-  //   if (prevYear === props.year) return
-  //   prevYear.current = props.year
-  // }
 
   const animateRender = () => {
     if (year === sYear) return
@@ -34,13 +26,12 @@ const CardContainer = props => {
       setSYear(year)
       setTense('future')
     } 
-
-
   }
 
   return (
     <div className="CardContainer" >
-      {animateRender()}
+  
+      {animateRender() /* trigger animation on render */}
       <div className={`Card ${tense}`}>
         <p>{CardObject[props.year]}</p> 
       </div>
